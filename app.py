04476a11e2c67a98d5d82cb98d1d697e31fc78d4
Onkar Jadhav <127065680+jadhav-onkar@users.noreply.github.com>
2025-06-16 11:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -281,5 +281,6 @@ def process_frames(camid, region, flag_r_zone=False, flag_pose_alert=False, flag
 
     cap.release()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # 10000 is Render's default
+    app.run(host='0.0.0.0', port=port)
